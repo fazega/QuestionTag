b54103a2bd7ba7d0e3487b79805bec55c26fa0ee
fazega <fazegame@hotmail.fr>
2016-06-14 16:22:13 +0200

    Rajout de la bdd dans backend/sql

--- backend/sql/questiontag.sql
+++ backend/sql/questiontag.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 10 Mai 2016 à 15:41
+-- Généré le :  Mar 14 Juin 2016 à 16:21
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -23,23 +23,94 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `chat_messages`
+--
+
+CREATE TABLE IF NOT EXISTS `chat_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_chat` int(11) NOT NULL,
+  `pseudo` varchar(100) NOT NULL,
+  `message` text NOT NULL,
+  `status_message` varchar(100) NOT NULL,
+  `date` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `chat_messages`
+--
+
+INSERT INTO `chat_messages` (`id`, `id_chat`, `pseudo`, `message`, `status_message`, `date`) VALUES
+(1, 1, 'fazega', 'yo', 'normal', '2016-06-12 22:06:20'),
+(2, 1, 'fazega', 'lol', 'normal', '2016-06-14 12:08:35'),
+(3, 1, 'fazega', 'treztrz', 'normal', '2016-06-14 12:56:03');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `chat_users`
+--
+
+CREATE TABLE IF NOT EXISTS `chat_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_chat` int(11) NOT NULL,
+  `pseudo` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `chat_users`
+--
+
+INSERT INTO `chat_users` (`id`, `id_chat`, `pseudo`, `status`) VALUES
+(1, 1, 'fazega', 'main'),
+(2, 1, 'jean-marc', 'normal');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `live_questions`
+--
+
+CREATE TABLE IF NOT EXISTS `live_questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `question` text NOT NULL,
+  `date` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `live_questions`
+--
+
+INSERT INTO `live_questions` (`id`, `id_user`, `question`, `date`) VALUES
+(1, 1, 'Quelles sont les réponses au qcm d''eco ? #polytechnique #eco', '2016-05-31 09:13:11');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(100) NOT NULL,
+  `pseudo` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
+  `mail` text NOT NULL,
   `creation_date` timestamp NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Contenu de la table `users`
 --
 
-INSERT INTO `users` (`id`, `login`, `password`, `creation_date`) VALUES
-(1, 'fazega', '0000', '2016-05-10 13:13:17');
+INSERT INTO `users` (`id`, `pseudo`, `password`, `mail`, `creation_date`) VALUES
+(1, 'fazega', '0000', 'fazegame@hotmail.fr', '2016-05-20 10:03:13'),
+(2, 'pad', '1111', 'pad@caca', '2016-05-20 10:03:32'),
+(3, 'allo123', 'caca', 'csdfezar', '2016-05-20 15:49:47');
 
 -- --------------------------------------------------------
 
